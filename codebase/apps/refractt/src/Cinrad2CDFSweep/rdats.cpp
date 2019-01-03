75beeb5c909e3cb63f2f7eae15bcc8abb3d6a1bd
zhaoping.sun <zhaoping.sun@metstar.net>
2019-01-03 11:25:13 +0800

    update

--- codebase/apps/refractt/src/Cinrad2CDFSweep/rdats.cpp
+++ codebase/apps/refractt/src/Cinrad2CDFSweep/rdats.cpp
@@ -7,7 +7,7 @@ size_t getSweepLength(TSSweepHeader *swphdr)
 	int chan=swphdr->chan;
 	if(chan==0) chan=1;
 	
-	return sizeof(*swphdr)+chan*swphdr->binnum*2*sizeof(float);
+	return sizeof(*swphdr)+chan*swphdr->binnum*2*sizeof(float)+swphdr->burstbinnum*2*sizeof(float);
 }
 TSSweepHeader *getNextSwpHeader(TSSweepHeader *swphdr)
 {
@@ -21,52 +21,45 @@ Iqcmpl *getIQData(TSSweepHeader *swphdr)
 vector<char> fileCache;
 int scanIQFile(const char*fname,TSHeader *tsh,SwpHdrList &swplist)
 {
+	int const IQ_START_POS=sizeof(TSHeader)+256;
 	FILE *fp=fopen(fname,"rb");
 	if(fp==NULL)
 		return 0;
-	fseek(fp,0,SEEK_END);
-    int fileLength=ftell(fp);
-	fileCache.resize(fileLength);
-	rewind(fp);
-	fread(&fileCache[0],1,fileLength,fp);
-/*
-	// 创建文件内核对象，其句柄保存于hFile
-	HANDLE hFile;
-	int error;
-	hFile = CreateFile((LPCSTR)fname,GENERIC_READ ,0,NULL,OPEN_ALWAYS,FILE_ATTRIBUTE_NORMAL,NULL);
-	if(hFile==INVALID_HANDLE_VALUE)
-	{
-		error=GetLastError();
-	}
-	 DWORD dwBytesInBlock,fileLength;
-	 dwBytesInBlock= GetFileSize(hFile,&fileLength);  
-	 if(INVALID_FILE_SIZE==dwBytesInBlock)
-	 {
-		error=GetLastError();
-	 }
-	//映射对象
-	HANDLE hFileMapping = CreateFileMapping(hFile,NULL,PAGE_READONLY,0,dwBytesInBlock,NULL);
-	
-	if (hFileMapping==NULL)
-	{
-		error=GetLastError();
-		return false;
-	}
-	//这里赋值了一个文件映射的指针，地址从文件头开始
-	TCHAR *pbFile = (TCHAR *)MapViewOfFile(hFileMapping, FILE_MAP_READ, 0, 0, 0);
-*/
+	fileCache.resize(IQ_START_POS);
+	fread(&fileCache[0],1,sizeof(TSHeader),fp);
 	char *pbFile=&fileCache[0];
 	TSHeader *IQFileHead=(TSHeader *)pbFile; 
-	memcpy(tsh,IQFileHead,sizeof(*tsh));
 
-	TSSweepHeader *SWFileHead =(TSSweepHeader*)(pbFile+sizeof(*IQFileHead)+256);
+	fseek(fp,0,SEEK_END);
+    	int fileLength=ftell(fp);
+
+	if(IQFileHead->version>=TS_VER_IQ16)
+	{
+		vector<char> tmpCache;
+		tmpCache.resize(fileLength);
+		fread(&tmpCache[0],1,fileLength,fp);
+		
+		fileLength=;
+		//decode sweep here
+		
+	}
+	else
+	{
+		rewind(fp);
+		fread(&fileCache[0],1,fileLength,fp);
+		memcpy(tsh,IQFileHead,sizeof(*tsh));
+
+	}
+
+
+	TSSweepHeader *SWFileHead =(TSSweepHeader*)(pbFile+IQ_START_POS);
 	TSSweepHeader *nexSWFileHead;
 	nexSWFileHead=SWFileHead;
 
 	size_t totalen=fileLength;
 	 
 	size_t datalen;
-	datalen=sizeof(IQFileHead)+256;
+	datalen=IQ_START_POS;
 
 	while(datalen<totalen)
 	{	
